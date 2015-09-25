f86221337dd9a0085af16d2778ff1b05caaf6fc7
julien <jr@corp.webedia.fr>
2015-09-25 13:52:05 +0200

    bugfix

--- Nf/Front/Request/Http.php
+++ Nf/Front/Request/Http.php
@@ -1,34 +1,34 @@
 <?php
-
 namespace Nf\Front\Request;
 
 class Http extends AbstractRequest
 {
 
-    protected $_params=array();
-    private $_put=null;
+    protected $_params = array();
+
+    private $_put = null;
 
     public function __construct()
     {
-        if (!empty($_SERVER['REDIRECT_URL'])) {
-            $uri=ltrim($_SERVER['REDIRECT_URL'], '/');
-            if (!empty($_SERVER['REDIRECT_QUERY_STRING'])) {
-                $uri.='?'.$_SERVER['REDIRECT_QUERY_STRING'];
+        if (! empty($_SERVER['REDIRECT_URL'])) {
+            $uri = ltrim($_SERVER['REDIRECT_URL'], '/');
+            if (! empty($_SERVER['REDIRECT_QUERY_STRING'])) {
+                $uri .= '?' . $_SERVER['REDIRECT_QUERY_STRING'];
             }
         } else {
-            $uri=ltrim($_SERVER['REQUEST_URI'], '/');
+            $uri = ltrim($_SERVER['REQUEST_URI'], '/');
         }
-        $this->_uri=$uri;
+        $this->_uri = $uri;
     }
 
     public function sanitizeUri()
     {
         // filter the uri according to the config of security.restrictCharactersInUrl
         // this option only allows us to use Alpha-numeric text, Tilde: ~, Period: ., Colon: :, Underscore: _, Dash: -
-        $config=\Nf\Registry::get('config');
+        $config = \Nf\Registry::get('config');
         if (isset($config->security->restrictCharactersInUrl) && $config->security->restrictCharactersInUrl) {
             if (preg_match('%[\w0-9~.,/@\-=:[\]{}|&?!\%]*%i', $this->_uri, $regs)) {
-                if ($this->_uri==$regs[0]) {
+                if ($this->_uri == $regs[0]) {
                     return true;
                 }
             }
@@ -71,32 +71,26 @@ class Http extends AbstractRequest
 
     public function getParams()
     {
-        $return       = $this->_params;
+        $return = $this->_params;
         $paramSources = $this->getParamSources();
-        if (in_array('_GET', $paramSources)
-            && isset($_GET)
-            && is_array($_GET)
-        ) {
+        if (in_array('_GET', $paramSources) && isset($_GET) && is_array($_GET)) {
             $return += $_GET;
         }
-        if (in_array('_POST', $paramSources)
-            && isset($_POST)
-            && is_array($_POST)
-        ) {
+        if (in_array('_POST', $paramSources) && isset($_POST) && is_array($_POST)) {
             $return += $_POST;
         }
         return $return;
     }
-
+    
     // get the string sent as put
     public function setPutFromRequest()
     {
-        if ($this->_put===null) {
+        if ($this->_put === null) {
             if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
-                $this->_put=file_get_contents("php://input");
+                $this->_put = file_get_contents("php://input");
             }
         } else {
-            $this->_put='';
+            $this->_put = '';
         }
     }
 
@@ -106,8 +100,8 @@ class Http extends AbstractRequest
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $post = file_get_contents("php://input");
         }
-
-        if ($jsonDecode=='assoc') {
+        
+        if ($jsonDecode == 'assoc') {
             return json_decode($post, true);
         } else {
             return $post;
@@ -116,47 +110,47 @@ class Http extends AbstractRequest
 
     public function getPut($jsonDecode = 'assoc')
     {
-        if ($jsonDecode=='assoc') {
+        if ($jsonDecode == 'assoc') {
             return json_decode($this->_put, true);
         } else {
             return $this->_put;
         }
     }
-
+    
     // handle the redirection according to the trailing slash configuration
     public function redirectForTrailingSlash()
     {
-        $config=\Nf\Registry::get('config');
-        $redirectionUrl=false;
-        $requestParams='';
-        $requestPage='/' . $this->_uri;
-
+        $config = \Nf\Registry::get('config');
+        $redirectionUrl = false;
+        $requestParams = '';
+        $requestPage = '/' . $this->_uri;
+        
         // we don't redirect for the home page...
-        if ($requestPage!='/' && mb_strpos($requestPage, '/?')!==0) {
+        if ($requestPage != '/' && mb_strpos($requestPage, '/?') !== 0) {
             // the url without the params is :
-            if (mb_strpos($requestPage, '?')!==false) {
-                $requestParams=mb_substr($requestPage, mb_strpos($requestPage, '?'), mb_strlen($requestPage) - mb_strpos($requestPage, '?'));
-                $requestPage=mb_substr($requestPage, 0, mb_strpos($requestPage, '?'));
+            if (mb_strpos($requestPage, '?') !== false) {
+                $requestParams = mb_substr($requestPage, mb_strpos($requestPage, '?'), mb_strlen($requestPage) - mb_strpos($requestPage, '?'));
+                $requestPage = mb_substr($requestPage, 0, mb_strpos($requestPage, '?'));
             }
-
-            if ($config->trailingSlash->needed==true) {
-                if (mb_substr($requestPage, -1, 1)!='/') {
-                    $redirectionUrl='http://' . $_SERVER['HTTP_HOST'] . $requestPage . '/' . $requestParams;
+            
+            if (isset($config->trailingSlash->needed) && $config->trailingSlash->needed==true) {
+                if (mb_substr($requestPage, - 1, 1) != '/') {
+                    $redirectionUrl = 'http://' . $_SERVER['HTTP_HOST'] . $requestPage . '/' . $requestParams;
                 }
             } else {
-                if (mb_substr($requestPage, -1, 1)=='/') {
-                    $redirectionUrl='http://' . $_SERVER['HTTP_HOST'] . rtrim($requestPage, '/') . $requestParams;
+                if (mb_substr($requestPage, - 1, 1) == '/') {
+                    $redirectionUrl = 'http://' . $_SERVER['HTTP_HOST'] . rtrim($requestPage, '/') . $requestParams;
                 }
             }
-
-            if ($redirectionUrl!==false) {
+            
+            if ($redirectionUrl !== false) {
                 $response = new \Nf\Front\Response\Http();
                 $response->redirect($redirectionUrl, 301);
                 $response->sendHeaders();
                 return true;
             }
         }
-
+        
         return false;
     }
 }
