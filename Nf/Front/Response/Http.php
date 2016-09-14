4a451a2044902b8953f56729738bcff4ead3b339
julien <jr@corp.webedia.fr>
2016-09-14 18:59:02 +0200

    setCacheable 0 for no-cache

--- Nf/Front/Response/Http.php
+++ Nf/Front/Response/Http.php
@@ -236,9 +236,16 @@ class Http extends AbstractResponse
     // sends header to allow the browser to cache the response a given time
     public function setCacheable($minutes)
     {
-        $this->setHeader('Expires', gmdate('D, d M Y H:i:s', time() + $minutes * 60) . ' GMT', true);
-        $this->setHeader('Cache-Control', 'max-age=' . $minutes * 60, true);
-        $this->setHeader('Pragma', 'public', true);
+        if($minutes <= 0) {
+            $this->setHeader('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+            $this->setHeader('Expires', '-1');
+            $this->setHeader('Pragma', 'no-cache');
+        }
+        else {
+            $this->setHeader('Expires', gmdate('D, d M Y H:i:s', time() + $minutes * 60) . ' GMT', true);
+            $this->setHeader('Cache-Control', 'max-age=' . $minutes * 60, true);
+            $this->setHeader('Pragma', 'public', true);
+        }
     }
 
     public function getContentType()
