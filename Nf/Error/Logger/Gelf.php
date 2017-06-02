74b515aa34be11dffc961591b2abf44126f399cd
julien <jr@corp.webedia.fr>
2017-06-02 16:25:19 +0200

    fix command line in gelf error report

--- Nf/Error/Logger/Gelf.php
+++ Nf/Error/Logger/Gelf.php
@@ -10,18 +10,18 @@ class Gelf
     public function log($err)
     {
         $config = Registry::get('config');
-        
+
         // We need a transport - UDP via port 12201 is standard.
         $transport = new \Gelf\Transport\UdpTransport($config->error->logger->gelf->ip, $config->error->logger->gelf->port, \Gelf\Transport\UdpTransport::CHUNK_SIZE_LAN);
-        
+
         // While the UDP transport is itself a publisher, we wrap it in a real Publisher for convenience
         // A publisher allows for message validation before transmission, and it calso supports to send messages
         // to multiple backends at once
         $publisher = new \Gelf\Publisher();
         $publisher->addTransport($transport);
-        
+
         $fullMessage = \Nf\Front\Response\Cli::displayErrorHelper($err);
-        
+
         // Now we can create custom messages and publish them
         $message = new \Gelf\Message();
         $message->setShortMessage(Handler::recursiveArrayToString($err['message']))
@@ -29,20 +29,22 @@ class Gelf
             ->setFile($err['file'])
             ->setLine($err['line'])
             ->setFullMessage($fullMessage);
-        
+
         if (php_sapi_name() == 'cli') {
             global $argv;
-            $message->setAdditional('url', 'su ' . $_SERVER['LOGNAME'] . ' -c "php ' . Registry::get('applicationPath') . '/html/' . implode(' ', $argv) . '"');
+            $argv2 = $argv;
+            unset($argv2[0]);
+            $message->setAdditional('url', 'su ' . $_SERVER['LOGNAME'] . ' -c "php ' . Registry::get('applicationPath') . '/html/index.php ' . implode(' ', $argv2) . '"');
         } else {
             $message->setAdditional('url', 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
         }
-        
+
         if (isset($config->error->logger->additionals)) {
             foreach ($config->error->logger->additionals as $additionalName => $additionalValue) {
                 $message->setAdditional($additionalName, $additionalValue);
             }
         }
-        
+
         if ($publisher->publish($message)) {
             return true;
         } else {
